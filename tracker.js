b026411e4f483a1508987c66f1129e13c6b99a60
Amin <aminahmedkhan@gmail.com>
2025-06-26 13:51:25 +0400

    update

--- tracker.js
+++ tracker.js
@@ -164,7 +164,6 @@
             browser_session_id: this.sessionId,
             browser_user_id: this.getUserId()
         };
-
     };
 
     PlatformanceTracker.prototype.calculateScrollInfo = function () {
